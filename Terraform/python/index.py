08777bfed6aef5e2a694b6cfadb86a7dea792956
Callum Ness <callumness@Callums-MacBook-Pro-2.local>
2023-11-12 16:59:04 +0000

    Tidying up py scipt

--- Terraform/python/index.py
+++ Terraform/python/index.py
@@ -6,36 +6,11 @@ import os
 
 def lambda_handler(event, context):
 
-    # profile = '***'
-    # region = 'eu-west-3'
+    s3 = boto3.client('s3')
+    dynamodb = boto3.client('dynamodb')
+    ssm = boto3.client('ssm')
 
-    # session = boto3.Session(profile_name=profile, region_name=region)
-
-    s3 = session.client('s3')
-    dynamodb = session.client('dynamodb')
-    sts = session.client('sts')
-    ssm = session.client('ssm')
-
-    parameter = ssm.get_parameter(Name=os.environ['DYNAMODB_NAME'])
-
-    PATH_IN_COMPUTER = '/Users/callumness/Documents/Python Personal/Github/AWS/src/rider_details.csv'
-
-    s3.put_object(
-        Bucket='csvprocessing',
-        Key='rider_details.csv',
-        Body=open(PATH_IN_COMPUTER, 'rb')
-    )
-
-    s3.get_object(
-        Bucket='csvprocessing',
-        Key='rider_details.csv',
-    )
-
-    # with open('src/rider_details.csv', newline='') as csvfile:
-    #     reader = csv.DictReader(csvfile)
-    #     for row in reader:
-    #         rows = row['Name']
-    #         print(rows)
+    parameter = ssm.get_parameter(Name=os.environ['SSMParameterName'])
 
     response = dynamodb.put_item(
         TableName="CSV_File_DB",
@@ -49,12 +24,6 @@ def lambda_handler(event, context):
         },
     )
 
-    with open('src/rider_details.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            row1 = row['Name']
-            # row2 = row['Brand']
-
     response = dynamodb.batch_write_item(
         RequestItems={
             'CSV_File_DB': [
@@ -62,10 +31,10 @@ def lambda_handler(event, context):
                     'PutRequest': {
                         'Item': {
                             'CSV': {
-                                'S': row1[0:1],
+                                'S': 'Test1',
                             },
                             'FileName': {
-                                'S': row1[:-1],
+                                'S': 'Test1',
                             }
                         },
                     },
